0c10c773753e3735ff0578c34d452e2102edb41e
wallace123 <bwtibbs@hotmail.com>
2024-05-17 19:42:07 +0000

    fixed the first test

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -12,18 +12,19 @@ Follow the instructions below to get started:
 '''
 
 from collections import namedtuple
+from decimal import Decimal
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
 def validorder(order: Order):
-    net = 0
+    net = Decimal(0)
 
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            net += Decimal(item.amount)
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            net -= Decimal(item.amount) * Decimal(item.quantity)
         else:
             return "Invalid item type: %s" % item.type
 
